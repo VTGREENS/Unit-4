8f0ffccbb0681d21b651921c1e7b06e0e67d34d0
VTGREENS <leifjaxson@gmail.com>
2022-12-09 12:37:35 -0500

    more commit

--- challenges/passing-data/greeting.js
+++ challenges/passing-data/greeting.js
@@ -1,1 +1,11 @@
+let friendGreeting = document.getElementById("friend-greeting");
+
+let queryString = window.location.search;
+
+let params = new URLSearchParams(queryString);
+
+let friendName = params.get("name");
+
+friendGreeting.textContent = "Hello " + friendName;
+
 
